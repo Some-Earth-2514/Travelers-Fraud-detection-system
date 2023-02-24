a5d023117c1486c663ecc4118a0f81fe22f24f17
Some-Earth-2514 <74248808+Some-Earth-2514@users.noreply.github.com>
2023-02-23 20:18:19 -0500

    Add files via upload

---
+++ import file.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+claim_cleaned = pd.read_csv(r"C:\Users\Samarth\Desktop\Traveler's Case Competition\Data sets\CVS\Claim cleaned.csv")
+print(claim_cleaned)
+
+claimant_cleaned = pd.read_csv(r"C:\Users\Samarth\Desktop\Traveler's Case Competition\Data sets\CVS\Claimant "
+                               r"cleaned.csv")
+print(claimant_cleaned)
