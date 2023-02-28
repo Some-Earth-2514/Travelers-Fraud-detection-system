e061a41a9b764be083f0ca1c42c5a868189062f2
priyarao2004 <101979575+priyarao2004@users.noreply.github.com>
2023-02-28 12:12:46 -0500

    Add files via upload

--- ALAE_fraud.py
+++ ALAE_fraud.py
@@ -7,7 +7,7 @@ for i in range(len(CLAIM_EXPENSE_EST_AMT)):
   # if VEH_PRIMARY_PT_OF_DAMAGE is not none and AUTO_ACCIDENT_DESC is multi 
   # multi by collision cost
   total_legal_fees = 0
-  total_legal_fees = BI_CLMT_CNT[i] * 18417 
+  total_legal_fees = BI_CLMT_CNT[i] * 15785 
   if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'No Damage': total_legal_fees += 0
   else: total_legal_fees += 4698 
   if CLAIM_EXPENSE_EST_AMT[i] > total_legal_fees: 
