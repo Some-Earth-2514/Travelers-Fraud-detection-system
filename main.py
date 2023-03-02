7b41d7b93c13feaa5ae5f4511a692aed6ef7cd19
Some-Earth-2514 <74248808+Some-Earth-2514@users.noreply.github.com>
2023-03-01 22:26:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ claim = pd.read_csv("Claim cleaned.csv")
 claimant = pd.read_csv("Claimant cleaned.csv")
 dict_claim = csv.DictReader(claim)
 dict_claimant = csv.DictReader(claimant)
+csv_claim = csv.reader("Claim cleaned.csv")
+csv_claimant = csv.reader("Claimant cleaned.csv")
+print(csv_claim)
 
 # converting column data to list
 
@@ -34,7 +37,9 @@ VEH_PRIMARY_PT_OF_DAMAGE = claimant['VEH_PRIMARY_PT_OF_DAMAGE'].tolist()
 AIRBAG_DEPLOYED = claimant['AIRBAG_DEPLOYED'].tolist()
 VEH_SPEED_AT_IMPACT = claimant['VEH_SPEED_AT_IMPACT'].tolist()
 
-junk_inital = set()
+junk_initial = set()
+clean_initial = set()
+fraud_initial = set()
 
 
 def read_datasets():
@@ -45,25 +50,12 @@ def print_datasets():
     pass
 
 
-def fraud():
-    pass
-
-
-def not_fraud():
-    pass
-
-
-def junk():
-    sorted_set = sorted(junk_inital)
-    print(sorted_set)
-
-    return junk
-
-
-def policy_state():
+def policy_state():  # done
     # appends the index of list 'POLICY_STATE' that is tested when counts are indexed
     junk_policy_state = []
-    count_num = 0
+    clean_policy_state = []
+    count_policy_state_junk = 0
+    count_policy_state_clean = 0
 
     # create a pandas Series from the list 'a'
     series_a = pd.Series(POLICY_STATE)
@@ -77,22 +69,32 @@ def policy_state():
     # iterates through count & check if true
     for i in range(len(count)):
         if count[i]:
-            count_num += 1
+            count_policy_state_junk += 1
             junk_policy_state.append(i)
+        else:
+            count_policy_state_clean += 1
+            clean_policy_state.append(i)
 
-    junk_inital.update(junk_policy_state)
+    junk_initial.update(junk_policy_state)
+    clean_initial.update(clean_policy_state)
 
-    print(count_num)
+    print(count_policy_state_junk)
     print(junk_policy_state)
-    print(junk_inital)
+    print(junk_initial)
+
+    print(count_policy_state_clean)
+    print(clean_policy_state)
+    print(clean_initial)
 
     return policy_state
 
 
 def birth_date():  # done
     # counters for ages below 18 and above 80
-    count = 0
-    junk_bd = []  # list holding index val. of fraud detected birthday
+    count_birth_date_junk = 0
+    count_birth_date_clean = 0
+    junk_birth_date = []  # list holding index val. of fraud detected birthday
+    clean_birth_date = []
 
     # iterate through the PRI_BTH_DT colm
     for i in range(len(PRI_BTH_DT)):
@@ -105,35 +107,191 @@ def birth_date():  # done
         # looping through for checking age bounds
         # total = 47
         if current_age < 18:
-            count += 1  # 47
-            junk_bd.append(i)
+            count_birth_date_junk += 1  # 47
+            junk_birth_date.append(i)
+        else:
+            count_birth_date_clean += 1
+            clean_birth_date.append(i)
 
-    junk_inital.update(junk_bd)
+    junk_initial.update(junk_birth_date)
+    clean_initial.update(clean_birth_date)
 
-    print(f"Below 18: {count}")
-    print(junk_bd)
-    print(junk_inital)
+    print(count_birth_date_junk)
+    print(f"Below 18: {junk_birth_date}")
+    print(junk_birth_date)
+    print(junk_initial)
+
+    print(count_birth_date_clean)
+    print(f"Above 18: {junk_birth_date}")
+    print(clean_birth_date)
+    print(clean_initial)
 
     return birth_date
 
 
 def vehicle_year():  # done
-    junk_veh_yr = []
+    junk_vehicle_year = []
+    clean_vehicle_year = []
 
-    count = 0
+    count_vehicle_year_junk = 0
+    count_vehicle_year_clean = 0
+
     # counts how many entries of one are greater than two
     for col in range(len(CLMT_VEH_YR)):
         if CLMT_VEH_YR[col] == 1900:
-            count += 1
-            junk_veh_yr.append(col)
+            count_vehicle_year_junk += 1
+            junk_vehicle_year.append(col)
+        else:
+            count_vehicle_year_clean += 1
+            clean_vehicle_year.append(col)
 
-    junk_inital.update(junk_veh_yr)
+    junk_initial.update(junk_vehicle_year)
+    clean_initial.update(clean_vehicle_year)
 
-    print(count)
-    print(junk_veh_yr)
-    print(junk_inital)
+    print(count_vehicle_year_junk)
+    print(junk_vehicle_year)
+    print(junk_initial)
+
+    print(count_vehicle_year_clean)
+    print(clean_vehicle_year)
+    print(clean_initial)
 
     return vehicle_year
+
+
+def junk():
+    junk_sorted = sorted(index + 1 for index in junk_initial)
+    print(junk_sorted)
+
+    # Read in the input file and remove the specified rows
+    with open("Claim cleaned.csv", 'r') as f_in_claim, open("JunkRemoved.csv", 'w', newline='') as f_out_junk:
+        reader = csv.reader(f_in_claim)
+        writer = csv.writer(f_out_junk)
+        for i, row in enumerate(reader):
+            if i not in junk_sorted:
+                writer.writerow(row)
+
+    return junk_sorted
+
+
+def clean():
+    clean_sorted = sorted(index + 1 for index in clean_initial)
+    print(clean_sorted)
+
+    # Read in the input file and remove the specified rows
+    with open("Claim cleaned.csv", 'r') as f_in_claim, open("CleanSorted.csv", 'w', newline='') as f_out_clean:
+        reader = csv.reader(f_in_claim)
+        writer = csv.writer(f_out_clean)
+        for i, row in enumerate(reader):
+            if i not in clean_sorted:
+                writer.writerow(row)
+
+    return clean_sorted
+
+
+def alea_fraud():
+    fraud_ALAE = []
+    count_ALAE = 0
+    total_legal_fees = 0
+
+    for i in range(len(CLAIM_EXPENSE_EST_AMT)):
+        # check bodily & multiple
+        # if VEH_PRIMARY_PT_OF_DAMAGE is not none and AUTO_ACCIDENT_DESC is multi
+        # multi by collision cost
+        total_legal_fees = BI_CLMT_CNT[i] * 15785
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'No Damage':
+            total_legal_fees += 0
+        else:
+            total_legal_fees += 4698
+        if CLAIM_EXPENSE_EST_AMT[i] > total_legal_fees:
+            fraud_ALAE.append(i)
+            count_ALAE += 1
+
+    fraud_initial.update(fraud_ALAE)
+
+    print(count_ALAE)
+    print(fraud_ALAE)
+    print(fraud_initial)
+
+    return alea_fraud
+
+
+def indemnity_fraud():
+    fraud_indemity = []
+    count = 0
+
+    for i in range(len(CLAIM_INDEMNITY_EST_AMT)):
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'All Over':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 118258.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Complete Drivers Side':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 49868.75:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Complete Front End':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 73086.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Complete Passenger Side':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 85506.50:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Complete Rear End':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 19917.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Drivers Center':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 83830.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Driver Front Corner':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 61479.75:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Driver Rear Corner':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 46557.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'No Damage':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 16612.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Passenger Center':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 58335.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Passenger Front Corner':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 48570.5:
+                fraud_indemity.append(i)
+        if VEH_PRIMARY_PT_OF_DAMAGE[i] == 'Passenger Rear Corner':
+            if CLAIM_INDEMNITY_EST_AMT[i] > 46471.5:
+                fraud_indemity.append(i)
+
+    for i in fraud_indemity:
+        count += 1
+
+    fraud_initial.update(fraud_indemity)
+
+    print(count)
+    print(fraud_indemity)
+    print(fraud_initial)
+
+    return indemnity_fraud
+
+
+def inspection_fraud():
+    fraud_inspections = []
+    count_inspections = 0
+
+    for i in range(len(APPR_DAM_EST_REC_CNT)):
+        if APPR_DAM_EST_REC_CNT[i] > 4:
+            fraud_inspections.append(i)
+            count_inspections += 1
+
+    fraud_initial.update(fraud_inspections)
+
+    print(count_inspections)
+    print(fraud_inspections)
+    print(fraud_initial)
+
+
+def fraud():
+    pass
+
+
+def not_fraud():
+    pass
 
 
 if __name__ == "__main__":
@@ -151,4 +309,20 @@ if __name__ == "__main__":
 
     print("Junk")
     junk()
+    print()
+
+    print("Clean")
+    clean()
+    print()
+
+    print("ALEA fraud")
+    alea_fraud()
+    print()
+
+    print("Indemnity fraud")
+    indemnity_fraud()
+    print()
+
+    print("Inspection fraud")
+    inspection_fraud()
     print()
